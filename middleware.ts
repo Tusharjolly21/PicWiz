b515b2e7bed105445b036a118a2edd3b33954d19
Tushar Jolly <tushar.gts7650@gmail.com>
2024-05-20 12:46:14 +0530

    changed authmiddleware to clerkmiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
-import { authMiddleware } from "@clerk/nextjs/server";
-export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/clerk"],
-});
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
